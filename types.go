52afe8c3f0a44351efe4fcf09cef37c57d56feb1
Adam S Levy <adam@aslevy.com>
2019-10-07 11:59:56 -0800

    Add ValueType and String

--- types.go
+++ types.go
@@ -39,3 +39,106 @@ const (
 	ValueTypeMultiValue_GUID                        = C.LIBPFF_VALUE_TYPE_MULTI_VALUE_GUID
 	ValueTypeMultiValueBinaryData                   = C.LIBPFF_VALUE_TYPE_MULTI_VALUE_BINARY_DATA
 )
+
+func (v ValueType) String() string {
+	switch v {
+	case ValueTypeUnspecified:
+		return "ValueTypeUnspecified"
+	case ValueTypeNull:
+		return "ValueTypeNull"
+	case ValueTypeInteger16bitSigned:
+		return "ValueTypeInteger16bitSigned"
+	case ValueTypeInteger32bitSigned:
+		return "ValueTypeInteger32bitSigned"
+	case ValueTypeFloat32bit:
+		return "ValueTypeFloat32bit"
+	case ValueTypeDouble64bit:
+		return "ValueTypeDouble64bit"
+	case ValueTypeCurrency:
+		return "ValueTypeCurrency"
+	case ValueTypeFloatingTime:
+		return "ValueTypeFloatingTime"
+	case ValueTypeError:
+		return "ValueTypeError"
+	case ValueTypeBoolean:
+		return "ValueTypeBoolean"
+	case ValueTypeObject:
+		return "ValueTypeObject"
+	case ValueTypeInteger_64bit_Signed:
+		return "ValueTypeInteger_64bit_Signed"
+	case ValueTypeStringASCII:
+		return "ValueTypeStringASCII"
+	case ValueTypeStringUnicode:
+		return "ValueTypeStringUnicode"
+	case ValueTypeFiletime:
+		return "ValueTypeFiletime"
+	case ValueTypeGUID:
+		return "ValueTypeGUID"
+	case ValueTypeServerIdentifier:
+		return "ValueTypeServerIdentifier"
+	case ValueTypeRestriction:
+		return "ValueTypeRestriction"
+	case ValueTypeRuleAction:
+		return "ValueTypeRuleAction"
+	case ValueTypeBinaryData:
+		return "ValueTypeBinaryData"
+	case ValueTypeMultiValueInteger16bitSigned:
+		return "ValueTypeMultiValueInteger16bitSigned"
+	case ValueTypeMultiValueInteger32bitSigned:
+		return "ValueTypeMultiValueInteger32bitSigned"
+	case ValueTypeMultiValueFloat32bit:
+		return "ValueTypeMultiValueFloat32bit"
+	case ValueTypeMultiValueDouble64bit:
+		return "ValueTypeMultiValueDouble64bit"
+	case ValueTypeMultiValueCurrency:
+		return "ValueTypeMultiValueCurrency"
+	case ValueTypeMultiValueFloatingtime:
+		return "ValueTypeMultiValueFloatingtime"
+	case ValueTypeMultiValueInteger64bitSigned:
+		return "ValueTypeMultiValueInteger64bitSigned"
+	case ValueTypeMultiValueStringASCII:
+		return "ValueTypeMultiValueStringASCII"
+	case ValueTypeMultiValueStringUnicode:
+		return "ValueTypeMultiValueStringUnicode"
+	case ValueTypeMultiValueFiletime:
+		return "ValueTypeMultiValueFiletime"
+	case ValueTypeMultiValue_GUID:
+		return "ValueTypeMultiValue_GUID"
+	case ValueTypeMultiValueBinaryData:
+		return "ValueTypeMultiValueBinaryData"
+	default:
+		return fmt.Sprintf("unrecognized ValueType: %v", uint32(v))
+	}
+
+}
+
+//LIBPFF_ITEM_TYPE_UNDEFINED,
+//LIBPFF_ITEM_TYPE_ACTIVITY,
+//LIBPFF_ITEM_TYPE_APPOINTMENT,
+//LIBPFF_ITEM_TYPE_ATTACHMENT,
+//LIBPFF_ITEM_TYPE_ATTACHMENTS,
+//LIBPFF_ITEM_TYPE_COMMON,
+//LIBPFF_ITEM_TYPE_CONFIGURATION,
+//LIBPFF_ITEM_TYPE_CONFLICT_MESSAGE,
+//LIBPFF_ITEM_TYPE_CONTACT,
+//LIBPFF_ITEM_TYPE_DISTRIBUTION_LIST,
+//LIBPFF_ITEM_TYPE_DOCUMENT,
+//LIBPFF_ITEM_TYPE_EMAIL,
+//LIBPFF_ITEM_TYPE_EMAIL_SMIME,
+//LIBPFF_ITEM_TYPE_FAX,
+//LIBPFF_ITEM_TYPE_FOLDER,
+//LIBPFF_ITEM_TYPE_MEETING,
+//LIBPFF_ITEM_TYPE_MMS,
+//LIBPFF_ITEM_TYPE_NOTE,
+//LIBPFF_ITEM_TYPE_POSTING_NOTE,
+//LIBPFF_ITEM_TYPE_RECIPIENTS,
+//LIBPFF_ITEM_TYPE_RSS_FEED,
+//LIBPFF_ITEM_TYPE_SHARING,
+//LIBPFF_ITEM_TYPE_SMS,
+//LIBPFF_ITEM_TYPE_SUB_ASSOCIATED_CONTENTS,
+//LIBPFF_ITEM_TYPE_SUB_FOLDERS,
+//LIBPFF_ITEM_TYPE_SUB_MESSAGES,
+//LIBPFF_ITEM_TYPE_TASK,
+//LIBPFF_ITEM_TYPE_TASK_REQUEST,
+//LIBPFF_ITEM_TYPE_VOICEMAIL,
+//LIBPFF_ITEM_TYPE_UNKNOWN
